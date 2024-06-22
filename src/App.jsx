d1b4bfc3ccb46ba2fbb7d05f32285b31e1e86687
Rokeya Sultana <rokeyasbdgmail.comgit config --global user.name Rokeya>
2024-06-22 11:21:22 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Footer/Footer'
+import Features from './Components/Features/Features'
+import AddNew from './Components/AddNew/AddNew'
+import Testimonials from './Components/Testimonials/Testimonials'
+import GetStarted from './Components/GetStarted/GetStarted'
+import Footer from './Components/Footer/Footer'
+
+function App () {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <AddNew />
+      <Testimonials />
+      <GetStarted />
+      <Footer />
+    </>
+  )
+}
+
+export default App
